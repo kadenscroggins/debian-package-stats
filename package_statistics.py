00410ab13a20cc4d14082f7f417284d7e10c1017
= <=>
2022-05-31 23:46:33 -0500

    Gets file and extracts it

--- package_statistics.py
+++ package_statistics.py
@@ -1,5 +1,7 @@
 import requests
 import sys
+import gzip
+import shutil
 
 # List of supported architectures
 allowed = ["all", "amd64", "arm64", "armel", "armhf", "i386", "mips64el", "mipsel", "ppc64el", "s390x"]
@@ -13,7 +15,12 @@ else: exit("Unknown architecture: " + package)
 
 url = "https://ftp.uk.debian.org/debian/dists/stable/main/Contents-" + package + ".gz"
 file = "./contents/" + package + ".gz"
+file_out = "./contents/" + package
 
 with open(file, 'wb') as f:
     resp = requests.get(url, verify=False)
     f.write (resp.content)
+
+with gzip.open(file, "rb") as f_in:
+    with open(file_out, "wb") as f_out:
+        shutil.copyfileobj(f_in, f_out)
